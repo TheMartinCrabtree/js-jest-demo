98464d3a664f1604b0089bb85c0ae50950bb25e0
MC-Games\MC <martin.crabtree@gmail.com>
2020-04-08 15:01:36 -0400

    changed test criteria to dogs and cats

--- _tests_/filterByTerm.spec.js
+++ _tests_/filterByTerm.spec.js
@@ -1,20 +1,26 @@
-describe("Test Name", ()=>{
-    test("Description of what is to be tested", ()=>{
+function filterByTerm(inputArr, keyword){
+    return inputArr.filter(function(inputElem){
+        return inputElem.animal.match(keyword);
+    });
+}
+
+describe("Cat Checker", ()=>{
+    test("Check if input contains the word CAT", ()=>{
         // array of test inputs
         const input = [
-            { id: 1, url: "https://www.url1.dev" },
-            { id: 2, url: "https://www.url2.dev" },
-            { id: 3, url: "https://www.link3.dev" }
+            { id: 1, animal: "good dog" },
+            { id: 2, animal: "good cat" },
+            { id: 3, animal: "bad CAT" }
         ];
 
         // expected output from the test
         const output = [
-            { id: 3, url: "https://www.link3.dev" }
+            { id: 3, animal: "bad CAT" }
         ];
 
         // running the test function that accepts the inputs 
         // check to see if the input contains the word "link"
         // and evaluate if the result matches the expected output
-        expect(filterByTerm(input, "link")).toEqual(output);
+        expect(filterByTerm(input, "CAT")).toEqual(output);
     });
 });
