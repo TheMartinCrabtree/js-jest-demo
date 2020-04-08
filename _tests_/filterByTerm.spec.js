fa69ab349286d697ded5f6c960bf160f62f72aa0
MC-Games\MC <martin.crabtree@gmail.com>
2020-04-08 14:41:41 -0400

    fixed package.json jest script reference

--- _tests_/filterByTerm.spec.js
+++ _tests_/filterByTerm.spec.js
@@ -1,9 +1,20 @@
 describe("Test Name", ()=>{
     test("Description of what is to be tested", ()=>{
+        // array of test inputs
         const input = [
             { id: 1, url: "https://www.url1.dev" },
             { id: 2, url: "https://www.url2.dev" },
             { id: 3, url: "https://www.link3.dev" }
         ];
+
+        // expected output from the test
+        const output = [
+            { id: 3, url: "https://www.link3.dev" }
+        ];
+
+        // running the test function that accepts the inputs 
+        // check to see if the input contains the word "link"
+        // and evaluate if the result matches the expected output
+        expect(filterByTerm(input, "link")).toEqual(output);
     });
 });
