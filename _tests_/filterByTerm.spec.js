686779633d9e62516fdd29d74ca1a010d772138d
MC-Games\MC <martin.crabtree@gmail.com>
2020-04-08 14:34:16 -0400

    writing out test cases

--- _tests_/filterByTerm.spec.js
+++ _tests_/filterByTerm.spec.js
@@ -1,6 +1,9 @@
-describe("Filter function", ()=>{
-    // test code goes here
-    // more info
-    let tracker = 0;
-
+describe("Test Name", ()=>{
+    test("Description of what is to be tested", ()=>{
+        const input = [
+            { id: 1, url: "https://www.url1.dev" },
+            { id: 2, url: "https://www.url2.dev" },
+            { id: 3, url: "https://www.link3.dev" }
+        ];
+    });
 });
